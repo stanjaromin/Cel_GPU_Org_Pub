d83b8f92f401536e1fcbb00e53b3cabc152b5a20
Preeti <131662865+PreetiTravisGPUSupport@users.noreply.github.com>
2023-05-05 14:54:46 +0530

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,35 +1,35 @@
 import time
-# #loop for more than 10 mins
+#loop for more than 10 mins
+# Set the duration to run the script
+DURATION = 11 * 60  # 11 minutes in seconds
+
+# Set the interval to print remaining time
+# Donot print anything until or > than 10 mins
+INTERVAL = 11 * 60  # 5 minutes in seconds
+
+# Get the start time
+start_time = time.time()
+
+# Run the script
+while (time.time() - start_time) < DURATION:
+    # Calculate the remaining time
+    remaining = DURATION - (time.time() - start_time)
+
+    # Check if it's time to print the remaining time
+    if (time.time() - start_time) >= INTERVAL:
+        print(f"Remaining time: {int(remaining/60)} minutes")
+        INTERVAL += (5*60)  # Update the interval
+
+    # Sleep for 1 minute
+    time.sleep(60)
+
+print("Script has finished running for 11 mins without printing anything.")
+
+
+
+# #loop for more than 65 mins
 # # Set the duration to run the script
-# DURATION = 11 * 60  # 11 minutes in seconds
-
-# # Set the interval to print remaining time
-# # Donot print anything until or > than 10 mins
-# INTERVAL = 11 * 60  # 5 minutes in seconds
-
-# # Get the start time
-# start_time = time.time()
-
-# # Run the script
-# while (time.time() - start_time) < DURATION:
-#     # Calculate the remaining time
-#     remaining = DURATION - (time.time() - start_time)
-
-#     # Check if it's time to print the remaining time
-#     if (time.time() - start_time) >= INTERVAL:
-#         print(f"Remaining time: {int(remaining/60)} minutes")
-#         INTERVAL += (5*60)  # Update the interval
-
-#     # Sleep for 1 minute
-#     time.sleep(60)
-
-# print("Script has finished running for 11 mins without printing anything.")
-
-
-
-# #loop for more than 50 mins
-# # Set the duration to run the script
-# DURATION = 51 * 60  # 50 minutes in seconds
+# DURATION = 65 * 60  # 50 minutes in seconds
 
 # # Set the interval to print remaining time
 # # Donot print anything until or > than 10 mins
@@ -51,31 +51,31 @@ import time
 #     # Sleep for 1 minute
 #     time.sleep(60)
 
-# print("Script has finished running for 51 mins by printing every 5 mins anything.")
+# print("Script has finished running for 65 mins by printing every 5 mins anything.")
 
 
-#loop for more than 180 mins
-# Set the duration to run the script
-DURATION = 181 * 60  # 11 minutes in seconds
+# #loop for more than 180 mins
+# # Set the duration to run the script
+# DURATION = 181 * 60  # 11 minutes in seconds
 
-# Set the interval to print remaining time
-# Donot print anything until or > than 10 mins
-INTERVAL = 5 * 60  # 5 minutes in seconds
+# # Set the interval to print remaining time
+# # Donot print anything until or > than 10 mins
+# INTERVAL = 5 * 60  # 5 minutes in seconds
 
-# Get the start time
-start_time = time.time()
+# # Get the start time
+# start_time = time.time()
 
-# Run the script
-while (time.time() - start_time) < DURATION:
-    # Calculate the remaining time
-    remaining = DURATION - (time.time() - start_time)
+# # Run the script
+# while (time.time() - start_time) < DURATION:
+#     # Calculate the remaining time
+#     remaining = DURATION - (time.time() - start_time)
 
-    # Check if it's time to print the remaining time
-    if (time.time() - start_time) >= INTERVAL:
-        print(f"Remaining time: {int(remaining/60)} minutes")
-        INTERVAL += (5*60)  # Update the interval
+#     # Check if it's time to print the remaining time
+#     if (time.time() - start_time) >= INTERVAL:
+#         print(f"Remaining time: {int(remaining/60)} minutes")
+#         INTERVAL += (5*60)  # Update the interval
 
-    # Sleep for 1 minute
-    time.sleep(60)
+#     # Sleep for 1 minute
+#     time.sleep(60)
 
-print("Script has finished running for 181 mins by printing every 5 mins")
+# print("Script has finished running for 181 mins by printing every 5 mins")
